1079f32e84d83956c939b72c54022e7cb76cf70f
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2023-12-01 09:41:07 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+LAB_INSTALL_DIR=~/labs
 install_configuration() {
 	echo "Installing packages"
 	sudo apt update -y
@@ -10,12 +10,13 @@ install_configuration() {
 	sudo snap install --classic code
 	sudo apt install -y python3 python3-pip
 	pip3 install flask
-	echo "installing labs in ~/labs"
+	echo "installing labs in $LAB_INSTALL_DIR"
 	cd ~
-	git clone https://github.com/KieranCrossland/cisco-labs-backup labs
+	git clone https://github.com/KieranCrossland/cisco-labs-backup $LAB_INSTALL_DIR
 	echo ""
 	echo "Cisco packet tracer is not in any repositories, you will have to install it seperately."
  	echo "install completed"
+  	xdg-open $LAB_INSTALL_DIR
 
 }
 
